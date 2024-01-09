35d5bc6d46bf422e96ae89837380f9e64ba1c0e9
Tobias Bertera <tobiasbertera@gmail.com>
2024-01-09 19:17:45 -0300

    Added DOM file and current and location functions
    the location and current functions tasks are the
    same of the other two (forecast and history).
    Desaggregate and structure the data.
    
    the DOM file is to start working in the UI.
    Currently working in the HTML and some basic
    CSS.

--- src/index.js
+++ src/index.js
@@ -15,22 +15,6 @@ let place = "q=Paris"
 
 async function getData(){
 
-    /// ONE BY ONE ///
-
-    /* let response = await fetch("http://api.weatherapi.com/v1/current.json?key=6401a6548a224689902171841233012&q=Buenos-Aires")
-    let data = await response.json()
-    console.log(data)
-
-    let response2 = await fetch("http://api.weatherapi.com/v1/forecast.json?key=6401a6548a224689902171841233012&q=Buenos-Aires&days=3")
-    let data2 = await response2.json()
-    console.log(data2)
- 
-    let response3 = await fetch("http://api.weatherapi.com/v1/history.json?key=6401a6548a224689902171841233012&q=Buenos-Aires&dt=2024-01-07")
-    let data3 = await response3.json()
-    console.log(data3)  */
-
-    /// WITH PROMISE.ALL() ///
-
 try{
     let [response, response2, response3] = await Promise.all([
         fetch("http://api.weatherapi.com/v1/current.json?key=6401a6548a224689902171841233012&q=Buenos-Aires"),
@@ -48,11 +32,13 @@ try{
     //compareObjects(data, data2, data3)
 
 
+    let weatherAndLocation = [getCurrentWeather(data), getLocation(data)]
     let forecastArr = getForecastData(data2)
     let historyArr = getHistoryData(data3)
 
     getStructuredForecast(forecastArr)
     getStructuredHistory(historyArr)
+    getWeatherAndLocation(weatherAndLocation)
 
 } catch(err){
         console.log(err)
@@ -68,10 +54,18 @@ function getStructuredForecast (arr){
 
 } 
 
-async function getStructuredHistory(arr) {
+function getStructuredHistory(arr) {
     let y = arr
     console.log(y)
 }
+
+function getWeatherAndLocation(arr){
+    let [currentWeather, location] = arr
+    console.log(currentWeather)
+    console.log(location)
+}
+
+
 
 getData()
 
