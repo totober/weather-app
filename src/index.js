531f3ae7166277beba5e903aa5646e1681cd2043
Tobias Bertera <tobiasbertera@gmail.com>
2024-02-02 11:17:51 -0300

    Added storeData() and retrieveData()
    this are the functions that the app was lacking.
    Now i dont need to fetch the data from the API
    every single time the user make a change in the
    interface.
    Now it just fetch data when the user search for
    another place.
    This was the more efficient way i was looking for.
    How i didnt think about this earlier? So obvious
    haha.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ let input = document.querySelector("input")
 let nav = document.querySelector("nav")
 let btnMenu = document.querySelector(".menu")
 let tempOpt = document.querySelector(".tempOpt")
+let theme = document.querySelector(".theme")
 
 input.addEventListener("keydown", getInputValue)
 btnMenu.addEventListener("click", showMenu)
 tempOpt.addEventListener("click", changeTemp)
+theme.addEventListener("click", changeTheme)
 window.addEventListener("click", closeMenu)
 window.addEventListener("load", init)
 
@@ -58,6 +60,7 @@ function getInputValue(e){
     getData(inputVal)
 
     //mainFunction(inputVal) 
+    //triggerDataDisplay()
 }
 
 
@@ -106,8 +109,6 @@ try{
     }  
 } 
 
-let body = document.body
-console.log(body)
 
 function changeTemp() {
 
@@ -121,10 +122,34 @@ function changeTemp() {
     if(this.classList.contains("fara")){
         this.textContent = "to Celsius"
     } else { 
-        this.textContent = "to Farenheit"
+        this.textContent = "to Fahrenheit"
     }
 
-    getData(placeVal, attr)
+    //getData(placeVal, attr)
+
+   let [location, forecastArr, historyArr] = retrieveData()
+   console.log("aca devolvemos la data")
+
+   triggerDataDisplay(location, forecastArr, historyArr, attr)
+
+   console.log("ERA GUARDAR LOS DATOS PAPAAAAAA")
+ 
+}
+
+function changeTheme(){
+
+    let body = document.body
+
+    if(body.classList.contains("theme-dark")){
+        body.classList.remove("theme-dark")
+        body.classList.add("theme-light")
+        this.textContent = "Dark theme"
+       
+    } else {
+        body.classList.remove("theme-light")
+        body.classList.add("theme-dark")
+        this.textContent = "Light theme"
+    }
 
 }
 
@@ -158,12 +183,30 @@ async function dataHandler(dataForecast, dataHistory, day){
 
     let [location, forecastArr, historyArr] = triggerDataStructuration(dataForecast, dataHistory)
 
+    storeData(location, forecastArr, historyArr)
+    console.log("aca store data")
+
     triggerDataDisplay(location, forecastArr, historyArr, day)
 }
 
 
 
+function storeData(location, forecastArr, historyArr) {
 
+    localStorage.setItem("location", JSON.stringify(location))
+    localStorage.setItem("forecastArr", JSON.stringify(forecastArr))
+    localStorage.setItem("historyArr", JSON.stringify(historyArr))
+
+}
+
+function retrieveData(){
+
+    let location = JSON.parse(localStorage.getItem("location"))
+    let forecastArr = JSON.parse(localStorage.getItem("forecastArr"))
+    let historyArr = JSON.parse(localStorage.getItem("historyArr"))
+
+    return [location, forecastArr, historyArr]
+}
 
 
 
